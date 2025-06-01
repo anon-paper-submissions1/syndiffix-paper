fde43ba067a1b861b55855a7679f431cbf8672d6
Paul Francis <paul@francis.com>
2025-06-01 14:15:58 +0200

    Update brm_attack.py

--- best_row_match/brm_attack.py
+++ best_row_match/brm_attack.py
@@ -216,7 +216,7 @@ def do_config():
         # Next populate with 3 random known column pairs
         all_column_pairs = list(itertools.combinations(df_orig.columns, 2))
         random.shuffle(all_column_pairs)
-        for known_column_pair in all_column_pairs[:3]:
+        for known_column_pair in all_column_pairs[:0]:
             # randomly select 3 secret columns that are not in known_column_pair
             columns = list(df_orig.columns)
             random.shuffle(columns)
@@ -234,7 +234,7 @@ def do_config():
         # Next populate with 3 random known column 3-column sets
         all_column_triples = list(itertools.combinations(df_orig.columns, 3))
         random.shuffle(all_column_triples)
-        for known_column_triple in all_column_triples[:3]:
+        for known_column_triple in all_column_triples[:0]:
             columns = list(df_orig.columns)
             random.shuffle(columns)
             secret_columns = [c for c in columns if c not in known_column_triple]
